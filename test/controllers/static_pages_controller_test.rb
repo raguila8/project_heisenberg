dabad354906b3969edb025d9bed559088fe49bf8
Rodrigo Aguilar <rodrigoaguilar887@gmail.com>
2017-09-06 13:41:43 -0700

    Add about page

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+		assert_select "title", "About | Project Heisenberg"
+  end
+
+end
