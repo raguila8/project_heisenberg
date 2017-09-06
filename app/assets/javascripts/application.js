e101d01a857829c98d43bff6725c2235380cb3d1
Rodrigo Aguilar <rodrigoaguilar887@gmail.com>
2017-09-06 15:37:16 -0700

    update site layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,6 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require jquery.min.js
+//= require bootstrap.min.js
 //= require_tree .
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
-    get static_pages_about_url
+    get root_path
     assert_response :success
 		assert_select "title", "About | Project Heisenberg"
   end
