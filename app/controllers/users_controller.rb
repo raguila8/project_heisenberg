d96dc9d7699001b700b6419b35bfd2faaecdcbe1
Rodrigo Aguilar <rodrigoaguilar887@gmail.com>
2017-10-23 23:26:38 -0700

    Administrative users can now delete other users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-	before_action :logged_in_user, only: [:index, :edit, :update, :show, :find_friends]
+	before_action :logged_in_user, only: [:index, :edit, :update, :show, :find_friends, :destroy]
 	before_action :correct_user,	 only: [:edit, :update]
+	before_action :admin_user, only: :destroy
 
 	def index
 		@friendships = current_user.friends.paginate(page: params[:page])
@@ -51,6 +52,12 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User deleted"
+		redirect_to find_friends_path
+	end
+
   private
 
 		def user_params
@@ -85,5 +92,10 @@ class UsersController < ApplicationController
 				kudos += post.cached_votes_up
 			end
 			return kudos
+		end
+
+		# Confirms an admin user.
+		def admin_user
+			redirect_to(root_url) unless current_user.admin?
 		end
 end
