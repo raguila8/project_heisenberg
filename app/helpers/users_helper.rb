85759a1590f371afa9cbe56cecbef1d1eb15686f
raguila8 <rodrigoaguilar887@gmail.com>
2018-04-05 15:22:34 -0700

    change autocomplete sql expression to satisfy PG in profuction

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -21,7 +21,7 @@ module UsersHelper
 	def search(pattern)
 		users_query = "(SELECT users.username AS label, users.id AS id, 'Users' AS category, '/default_profile_image.png' AS image_url FROM users WHERE users.username LIKE '#{pattern}' LIMIT 5) "
 		problems_query = "(SELECT problems.title AS label, problems.id AS id, 'Problems' AS category, '/problem_icon.png' AS image_url FROM problems WHERE problems.title LIKE '#{pattern}' LIMIT 5)"
-		query = "SELECT * FROM #{users_query} UNION SELECT * FROM #{problems_query}"
+		query = "SELECT * FROM #{users_query} AS users_query UNION SELECT * FROM #{problems_query} AS problems_query"
 		suggestions = ActiveRecord::Base.connection.execute(query)
 		return suggestions
 	end
