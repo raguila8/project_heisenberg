4d161bb83c037aa6f78011cfe5e162b6fc560a54
raguila8 <rodrigoaguilar887@gmail.com>
2018-04-05 15:31:40 -0700

    Postgresql error: 'failed to find conversion function from unknown to text' for autocomplete search fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -19,8 +19,13 @@ module UsersHelper
 	end
 
 	def search(pattern)
-		users_query = "(SELECT users.username AS label, users.id AS id, 'Users' AS category, '/default_profile_image.png' AS image_url FROM users WHERE users.username LIKE '#{pattern}' LIMIT 5) "
-		problems_query = "(SELECT problems.title AS label, problems.id AS id, 'Problems' AS category, '/problem_icon.png' AS image_url FROM problems WHERE problems.title LIKE '#{pattern}' LIMIT 5)"
+		if Rails.env.development? || Rails.env.test?
+			users_query = "(SELECT users.username AS label, users.id AS id, 'Users' AS category, '/default_profile_image.png' AS image_url FROM users WHERE users.username LIKE '#{pattern}' LIMIT 5) "
+			problems_query = "(SELECT problems.title AS label, problems.id AS id, 'Problems' AS category, '/problem_icon.png' AS image_url FROM problems WHERE problems.title LIKE '#{pattern}' LIMIT 5)"
+		else
+			users_query = "(SELECT users.username AS label, users.id AS id, 'Users' AS category, cast('/default_profile_image.png' as text) AS image_url FROM users WHERE users.username LIKE '#{pattern}' LIMIT 5) "
+			problems_query = "(SELECT problems.title AS label, problems.id AS id, 'Problems' AS category, cast('/problem_icon.png' as text) AS image_url FROM problems WHERE problems.title LIKE '#{pattern}' LIMIT 5)"
+		end
 		query = "SELECT * FROM #{users_query} AS users_query UNION SELECT * FROM #{problems_query} AS problems_query"
 		suggestions = ActiveRecord::Base.connection.execute(query)
 		return suggestions
